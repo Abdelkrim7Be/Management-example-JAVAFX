4645c644a986a440b416162d6d2657f0de70c944
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-01-07 19:40:58 +0100

    Add required dependencies for MySQL, JavaFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module com.abdelkrim.management_template {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
+// Export the packages to be used by other modules
+    exports com.abdelkrim.management_template.dao;
+    exports com.abdelkrim.management_template.models;
+    exports com.abdelkrim.management_template.presentation.javafx;
+    exports com.abdelkrim.management_template.presentation.javafx.controllers;
 
-    opens com.abdelkrim.management_template to javafx.fxml;
-    exports com.abdelkrim.management_template;
+    // Open the package for reflection (needed by JavaFX FXML)
+    opens com.abdelkrim.management_template.presentation.javafx.views to javafx.fxml;
+    opens com.abdelkrim.management_template.presentation.javafx.controllers to javafx.fxml;
+    opens com.abdelkrim.management_template.models to javafx.base;
 }
