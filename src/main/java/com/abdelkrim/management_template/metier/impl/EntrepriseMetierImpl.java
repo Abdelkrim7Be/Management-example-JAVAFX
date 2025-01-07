678eaa3e8e9392179e3a1a1977ca79b8d5079460
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-01-07 22:48:12 +0100

    Business Logic Layer : EntrepriseMetier Interface and its implementation EntrepriseDaoImpl Implemented

--- src/main/java/com/abdelkrim/management_template/metier/impl/EntrepriseMetierImpl.java
+++ src/main/java/com/abdelkrim/management_template/metier/impl/EntrepriseMetierImpl.java
@@ -1,4 +1,80 @@
 package com.abdelkrim.management_template.metier.impl;
 
-public class EntrepriseMetierImpl {
+import com.abdelkrim.management_template.dao.interfaces.EntrepriseDao;
+import com.abdelkrim.management_template.models.Entreprise;
+import com.abdelkrim.management_template.metier.interfaces.IEntrepriseMetier;
+
+import java.util.List;
+
+public class EntrepriseMetierImpl implements IEntrepriseMetier {
+
+    private final EntrepriseDao EntrepriseDao;
+
+    public EntrepriseMetierImpl(EntrepriseDao entrepriseDao) {
+        this.EntrepriseDao = entrepriseDao;
+    }
+
+    @Override
+    public void save(Entreprise entreprise) throws Exception {
+        if (entreprise == null) {
+            throw new Exception("Entreprise cannot be null");
+        }
+        EntrepriseDao.save(entreprise);
+    }
+
+    @Override
+    public List<Entreprise> findAll() throws Exception {
+        return EntrepriseDao.findAll();
+    }
+
+    @Override
+    public Entreprise findById(int id) throws Exception {
+        Entreprise entreprise = EntrepriseDao.findById(id);
+        if (entreprise == null) {
+            throw new Exception("Entreprise with ID " + id + " not found");
+        }
+        return entreprise;
+    }
+
+    @Override
+    public Entreprise findByName(String name) throws Exception {
+        Entreprise entreprise = EntrepriseDao.findByName(name);
+        if (entreprise == null) {
+            throw new Exception("Entreprise with name " + name + " not found");
+        }
+        return entreprise;
+    }
+
+    @Override
+    public List<Entreprise> findByKeyword(String keyword) throws Exception {
+        return EntrepriseDao.findByKeyword(keyword);
+    }
+
+    @Override
+    public List<Entreprise> findDepartmentsByEntrepriseId(int entrepriseId) throws Exception {
+        return EntrepriseDao.findDepartmentsByEntrepriseId(entrepriseId);
+    }
+
+    @Override
+    public void delete(int id) throws Exception {
+        Entreprise entreprise = EntrepriseDao.findById(id);
+        if (entreprise == null) {
+            throw new Exception("Entreprise with ID " + id + " does not exist");
+        }
+        EntrepriseDao.delete(id);
+    }
+
+    @Override
+    public void update(Entreprise entreprise) throws Exception {
+        if (entreprise == null) {
+            throw new Exception("Entreprise cannot be null");
+        }
+
+        Entreprise existingEntreprise = EntrepriseDao.findById(entreprise.getId());
+        if (existingEntreprise == null) {
+            throw new Exception("Entreprise with ID " + entreprise.getId() + " not found");
+        }
+
+        EntrepriseDao.update(entreprise); 
+    }
 }
--- src/main/java/com/abdelkrim/management_template/metier/interfaces/IEntrepriseMetier.java
+++ src/main/java/com/abdelkrim/management_template/metier/interfaces/IEntrepriseMetier.java
@@ -1,4 +1,16 @@
 package com.abdelkrim.management_template.metier.interfaces;
 
-public class IEntrepriseMetier {
+import com.abdelkrim.management_template.models.Entreprise;
+
+import java.util.List;
+
+public interface IEntrepriseMetier {
+    void save(Entreprise entreprise) throws Exception;
+    List<Entreprise> findAll() throws Exception;
+    Entreprise findById(int id) throws Exception;
+    Entreprise findByName(String name) throws Exception;
+    List<Entreprise> findByKeyword(String keyword) throws Exception;
+    List<Entreprise> findDepartmentsByEntrepriseId(int entrepriseId) throws Exception;
+    void update(Entreprise entreprise) throws Exception;
+    void delete(int id) throws Exception;
 }
