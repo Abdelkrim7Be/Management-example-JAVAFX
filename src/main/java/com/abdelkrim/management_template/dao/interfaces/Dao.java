ca982f05d16317b7acd9f4b8b3934fc81ff1e770
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-01-07 20:19:55 +0100

    Add generic DAO interface with CRUD operations

--- src/main/java/com/abdelkrim/management_template/dao/interfaces/Dao.java
+++ src/main/java/com/abdelkrim/management_template/dao/interfaces/Dao.java
@@ -1,4 +1,17 @@
 package com.abdelkrim.management_template.dao.interfaces;
 
-public class Dao {
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Dao<T> {
+
+    void create(T entity) throws SQLException;
+
+    List<T> findAll() throws SQLException;
+
+    T findById(int id) throws SQLException;
+
+    void update(T entity) throws SQLException;
+
+    void delete(int id) throws SQLException;
 }
