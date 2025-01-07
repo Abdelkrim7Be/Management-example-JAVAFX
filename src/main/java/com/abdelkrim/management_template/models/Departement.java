bfc0dc32e48fa3d494cb2a0ba04a2a0ae24bf506
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-01-07 21:34:48 +0100

    Models classes Implemented

--- src/main/java/com/abdelkrim/management_template/models/Departement.java
+++ src/main/java/com/abdelkrim/management_template/models/Departement.java
@@ -1,4 +1,40 @@
 package com.abdelkrim.management_template.models;
 
 public class Departement {
+    private int id;
+    private String nom;
+    private int entrepriseId;
+
+    public Departement(int id, String nom, int entrepriseId) {
+        this.id = id;
+        this.nom = nom;
+        this.entrepriseId = entrepriseId;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public int getEntrepriseId() {
+        return entrepriseId;
+    }
+    public void setEntrepriseId(int entrepriseId) {
+        this.entrepriseId = entrepriseId;
+    }
+    @Override
+    public String toString() {
+        return "Departement{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", entrepriseId=" + entrepriseId +
+                '}';
+    }
 }
