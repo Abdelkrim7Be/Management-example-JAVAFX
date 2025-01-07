e1d374414112e0cd415e7bdad0a70c31dbb885ec
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-01-07 20:09:45 +0100

    Implement SingletonConnexionDB for database connection

--- src/main/java/com/abdelkrim/management_template/dao/SingletonConnexionDB.java
+++ src/main/java/com/abdelkrim/management_template/dao/SingletonConnexionDB.java
@@ -1,4 +1,29 @@
 package com.abdelkrim.management_template.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SingletonConnexionDB {
+
+    private static Connection connection;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/employes_management";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private SingletonConnexionDB() {}
+
+    public static Connection getConnexion() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("On est connecté à la base de données.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Une erreur s'est produite lors de la connexion à la BD");
+            }
+        }
+        return connection;
+    }
 }
