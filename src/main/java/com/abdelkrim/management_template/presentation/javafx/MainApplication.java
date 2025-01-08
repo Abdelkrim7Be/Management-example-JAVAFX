e9272ccf067615ff99b43a843fbb707cda8202c0
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-01-08 15:24:13 +0100

    Setup basic JavaFX application structure

--- src/main/java/com/abdelkrim/management_template/presentation/javafx/MainApplication.java
+++ src/main/java/com/abdelkrim/management_template/presentation/javafx/MainApplication.java
@@ -1,4 +1,23 @@
 package com.abdelkrim.management_template.presentation.javafx;
 
-public class MainApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/abdelkrim/management_template/presentation/javafx/views/entreprise.fxml"));
+
+        primaryStage.setTitle("Employe Management Application");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
